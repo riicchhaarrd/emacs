5e592c05927802eac14a7568f25a85620bc73a17
Richard <riicchhaarrd@users.noreply.github.com>
2021-07-02 05:00:09 +0200

    reordering

--- init.el
+++ init.el
@@ -1,3 +1,26 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(cua-mode t nil (cua-base))
+ '(custom-enabled-themes '(zenburn))
+ '(custom-safe-themes
+   '("e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "ae5b216c8bf4c27e6de9cc7627e5fac03915fe1a5ce5c35eacb8860fa4a4cb94" "36d890facd489128e70af97d73899d0a4cbab7c8e6971f7dba64a6e7764fcaa0" "51ba8411a3c669279cba2e3d35d6a260986e95e57a9734bdd6c23af658117429" default))
+ '(horizontal-scroll-bar-mode nil)
+ '(package-selected-packages
+   '(ccls magit zenburn-theme solarized-theme gruvbox-theme rainbow-delimiters highlight-numbers plantuml-mode org-download org-superstar org-ref ox-twbs org-bullets yasnippet-snippets lsp-ui evil company-lsp use-package treemacs naysayer-theme clang-format which-key))
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 10)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas for Powerline" :foundry "MS  " :slant normal :weight normal :height 103 :width normal))))
+ '(minibuffer-prompt ((t (:background "black" :foreground "#F0DFAF" :weight bold)))))
+
 ;; https://stackoverflow.com/questions/2079095/how-to-modularize-an-emacs-configuration/2079146
 
 (defconst user-init-dir
@@ -26,4 +49,4 @@
 (load-user-file "shortcuts-packages.el")
 (load-user-file "treemacs.el")
 
-(server-start) ;; so we can use a bash script and windmove using emacsclient
+;; (server-start) ;; so we can use a bash script and windmove using emacsclient
