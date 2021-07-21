2605165fc80781b5e1738fb0e44967939d7c80f5
Richard <riicchhaarrd@users.noreply.github.com>
2021-07-21 17:24:07 +0200

    Theme changes

--- init.el
+++ init.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(cua-mode t nil (cua-base))
- '(custom-enabled-themes '(zenburn))
+ '(custom-enabled-themes '(liteblu))
  '(custom-safe-themes
-   '("f5e984f4dd51243ad7eeebc318f3338a84ff3b86718ae9e62f3acf9ab877e94f" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "d14f3df28603e9517eb8fb7518b662d653b25b26e83bd8e129acea042b774298" "ae5b216c8bf4c27e6de9cc7627e5fac03915fe1a5ce5c35eacb8860fa4a4cb94" "36d890facd489128e70af97d73899d0a4cbab7c8e6971f7dba64a6e7764fcaa0" "51ba8411a3c669279cba2e3d35d6a260986e95e57a9734bdd6c23af658117429" default))
+   '("f5e984f4dd51243ad7eeebc318f3338a84ff3b86718ae9e62f3acf9ab877e94f" "e6df46d5085fde0ad56a46ef69ebb388193080cc9819e2d6024c9c6e27388ba9" "cbdcdfe96e7c6ff03439fcefc964ed5e6b071d558088b852243dffe3b93c9df4" default))
  '(ediff-split-window-function 'split-window-horizontally t)
  '(horizontal-scroll-bar-mode nil)
  '(menu-bar-mode nil)
